8f1ace68abafbe061428afdff709035d8fc89677
Raul Garcia <punkvomit21@gmail.com>
2021-05-19 22:00:42 -0400

    set up for project

--- src/App.js
+++ src/App.js
@@ -1,11 +1,10 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="blog-post-project">
       <header className="App-header">
-
+        <h1>Start Project</h1>
       </header>
     </div>
   );
