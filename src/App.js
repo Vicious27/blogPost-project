c9e4c2011105dd2dd4e7f34bc51777091066f461
Raul Garcia <punkvomit21@gmail.com>
2021-05-20 00:30:50 -0400

    added state using hooks to app component and imported Posts

--- src/App.js
+++ src/App.js
@@ -1,8 +1,26 @@
-import React from "react";
-import "./App.css";
+import React, { useState } from "react";
 import Header from "./components/Header";
+import Posts from "./components/Posts";
+import "./App.css";
 
 const App = (props) => {
+  const [posts, setPosts] = useState([
+    {
+      id: 1,
+      title: "Hello React",
+      content: "lorem.",
+    },
+    {
+      id: 2,
+      title: "Hellow Project",
+      content: "tothe.",
+    },
+    {
+      id: 3,
+      title: "Hello Blog",
+      content: "Ipsum.",
+    }
+  ]);
 
   return (
     <div className="App">
