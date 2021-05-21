36545a9d631e7a9d37dac6976384b9b373edb76b
Raul Garcia <punkvomit21@gmail.com>
2021-05-21 02:38:04 -0400

    added Link to Posts and Header Component

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 
 const Header = (props) => {
@@ -6,7 +7,9 @@ const Header = (props) => {
   return (
     <header className="App-header">
       <ul className="container">
-        <li>Site Title</li>
+        <li key="home">
+          <Link to="/">Site Title</Link>
+        </li>
       </ul>
     </header>
   )
--- src/components/Posts.js
+++ src/components/Posts.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 const Posts = ({ posts }) => {
 
@@ -10,7 +11,11 @@ const Posts = ({ posts }) => {
           No posts yet!</li>)}
         {posts.map(post => (
           <li key={post.id}>
-            <h2>{post.title}</h2>
+            <h2>
+              <Link to={`/post/${post.slug}`}>
+                {post.title}
+              </Link>
+            </h2>
           </li>
         ))}
       </ul>
