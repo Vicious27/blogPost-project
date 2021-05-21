36bbd26da46bae993aaf9167ec2704193c065f7c
Raul Garcia <punkvomit21@gmail.com>
2021-05-21 02:10:02 -0400

    added slugs to state

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, { useState } from "react";
+import { BrowserRouter as Router, Switch, Route, Redirect } from "react-router-dom";
 import Header from "./components/Header";
 import Posts from "./components/Posts";
 import "./App.css";
@@ -7,16 +8,19 @@ const App = (props) => {
   const [posts, setPosts] = useState([
     {
       id: 1,
+      slug: "Hello React",
       title: "Hello React",
       content: "lorem.",
     },
     {
       id: 2,
+      slug: "Hello Project",
       title: "Hello Project",
       content: "tothe.",
     },
     {
       id: 3,
+      slug: "Hello Blog",
       title: "Hello Blog",
       content: "Ipsum.",
     }
