a961f2c0caab96a5e1892b4141eb695425c1fb86
Raul Garcia <punkvomit21@gmail.com>
2021-05-27 15:01:31 -0400

    set up config and firebase initialization/exporting

--- src/firebase.js
+++ src/firebase.js
@@ -2,7 +2,16 @@ import * as firebase from "firebase/app";
 import "firebase/auth";
 import "firebase/database";
 
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const config = {
+  apiKey: "AIzaSyASYpWZaZoIyymdMH565FAW9SsSxLyTPXo",
+  authDomain: "skate-sesh-blog.firebaseapp.com",
+  projectId: "skate-sesh-blog",
+  storageBucket: "skate-sesh-blog.appspot.com",
+  messagingSenderId: "17887962957",
+  appId: "1:17887962957:web:0fec9786da80dc0f003904",
+  measurementId: "G-DR0TVM6GZB"
+};
 //firebase initialization and exporting
-const config = {};
 firebase.initializeApp(config);
 export default firebase;
